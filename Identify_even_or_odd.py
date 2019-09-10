823a51bb4387e5347d726259cec9171b442e7d5d
Anil Patidar <patidaranil0791@gmail.com>
2019-09-10 15:19:52 +0530

    Update Identify_even_or_odd.py

--- Identify_even_or_odd.py
+++ Identify_even_or_odd.py
@@ -1,3 +1,12 @@
+
+# Write a function called showNumbers that takes a parameter called limit. It should print all the numbers between 0 and limit with a label
+# to identify the even and odd numbers. For example, if the limit is 3, it should print:
+# OUTPUT AS:
+# 1.ODD
+# 2.EVEN
+# 3.ODD
+===========================================================================================================================================
+# CODE:
 def showNumber(limit):
     for i in range(1,limit+2):
         if i%2==0:
