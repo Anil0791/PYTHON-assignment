8205da26dcc3b45aad488727ff551e5910859814
Anil Patidar <patidaranil0791@gmail.com>
2019-09-18 16:12:10 +0530

    Update Assignment_1_Solutions.py

--- Assignment_1_Solutions.py
+++ Assignment_1_Solutions.py
@@ -459,6 +459,10 @@ ANSWER:
 =======================================================================================================================================
 
 Question 21.
+Write a program that accepts a sentence and calculate the number of upper case letters and lower case letters.
+Suppose the following input is supplied to the program: Hello world! Then, the output should be: UPPER CASE 1 LOWER CASE 9
+Hints:
+In case of input data being supplied to the question, it should be assumed to be a console input.
 ---------------------------------------------------------------------------------------------------------------------------------------
 
 ANSWER:
@@ -466,6 +470,10 @@ ANSWER:
 =======================================================================================================================================
 
 Question 22.
+Write a program that computes the value of a+aa+aaa+aaaa with a given digit as the value of a.
+Suppose the following input is supplied to the program: 9 Then, the output should be: 11106
+Hints:
+In case of input data being supplied to the question, it should be assumed to be a console input.
 ---------------------------------------------------------------------------------------------------------------------------------------
 
 ANSWER:
@@ -473,6 +481,11 @@ ANSWER:
 =======================================================================================================================================
 
 Question 23.
+Use a list comprehension to square each odd number in a list. The list is input by a sequence of comma-separated numbers.
+Suppose the following input is supplied to the program: 1,2,3,4,5,6,7,8,9 Then, the output should be: 1,3,5,7,9
+Hints:
+In case of input data being supplied to the question, it should be assumed to be a console input.
+
 ---------------------------------------------------------------------------------------------------------------------------------------
 
 ANSWER:
@@ -480,6 +493,12 @@ ANSWER:
 =======================================================================================================================================
 
 Question 24.
+Write a program that computes the net amount of a bank account based a transaction log from console input. 
+The transaction log format is shown as following: D 100 W 200
+D means deposit while W means withdrawal. Suppose the following input is supplied to the program: D 300 D 300 W 200 D 100
+Then, the output should be: 500
+Hints:
+In case of input data being supplied to the question, it should be assumed to be a console input.
 ---------------------------------------------------------------------------------------------------------------------------------------
 
 ANSWER:
@@ -488,6 +507,24 @@ ANSWER:
 =======================================================================================================================================
 
 Question 25.
+A website requires the users to input username and password to register. 
+Write a program to check the validity of password input by users. Following are the criteria for checking the password:
+1. At least 1 letter between [a-z]
+2. At least 1 number between [0-9]
+3. At least 1 letter between [A-Z]
+4. At least 1 character from [$#@]
+5. Minimum length of transaction password: 6
+6. Maximum length of transaction password: 12 Your program should accept a sequence of comma separated passwords 
+   and will check them according to the above criteria. Passwords that match the criteria are to be printed, each separated by a comma.
+
+Example : If the following passwords are given as input to the program:
+ABd1234@1,a F1#,2w3E*,2We3345
+                              
+Then, the output of the program should be:
+ABd1234@1
+                              
+Hints:
+In case of input data being supplied to the question, it should be assumed to be a console input.
 ---------------------------------------------------------------------------------------------------------------------------------------
 
 ANSWER:
@@ -495,6 +532,15 @@ ANSWER:
 =======================================================================================================================================
 
 Question 26.
+You are required to write a program to sort the (name, age, height) tuples by ascending order where name is string, 
+age and height are numbers.The tuples are input by console. The sort criteria is: 1: Sort based on name; 2: Then sort based on age; 3: 
+Then sort by score. The priority is that name > age > score. 
+If the following tuples are given as input to the program: Tom,19,80 John,20,90 Jony,17,91 Jony,17,93 Json,21,85
+Then, the output of the program should be:
+[('John', '20', '90'), ('Jony', '17', '91'), ('Jony', '17', '93'), ('Json', '21', '85'), ('Tom', '19', '80')]
+Hints:
+In case of input data being supplied to the question, it should be assumed to be a console input.
+We use itemgetter to enable multiple sort keys.                             
 ---------------------------------------------------------------------------------------------------------------------------------------
 
 ANSWER:
@@ -502,6 +548,7 @@ ANSWER:
 =======================================================================================================================================
 
 Question 27.
+
 ---------------------------------------------------------------------------------------------------------------------------------------
 
 ANSWER:
