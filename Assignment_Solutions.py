55282e6f6bdf73e51fc9c6cf20d13f2816d1b9da
Anil Patidar <patidaranil0791@gmail.com>
2019-09-18 14:04:26 +0530

    Update Assignment_Solutions.py

--- Assignment_Solutions.py
+++ Assignment_Solutions.py
@@ -147,14 +147,16 @@ prime(limit)
 =======================================================================================================================================
 
 Question 8.
-Write a program which will find all such numbers which are divisible by 7 but are not a multiple of 5, between 2000 and 3200 (both included). The numbers obtained should be printed in a comma-separated sequence on a single line.
+Write a program which will find all such numbers which are divisible by 7 but are not a multiple of 5, 
+between 2000 and 3200 (both included). The numbers obtained should be printed in a comma-separated sequence on a single line.
 Hints:
 Consider use range(begin, end) method.
 ---------------------------------------------------------------------------------------------------------------------------------------
 
 ANSWER:
 =======
-************* with function************
+************* with function****************************
+
 def show(x,y):
       L=[]
       for i in range(x,y+1):
@@ -167,12 +169,178 @@ def show(x,y):
 x=2000;y=3000
 show(x,y)
 
+-------------------------------------------------------------------------
+
 ************** without function ************************
+
 l1=[]
 for i in range(2000,3001):
       if i%7==0 and i%5!=0:
             l1.append(i)
 print(",".join(map(str,l1)))
+
+=======================================================================================================================================
+
+Question 9.
+Write a program which can compute the factorial of a given numbers.
+The results should be printed in a comma-separated sequence on a single line. Suppose the following input is supplied to the program: 8
+Then, the output should be: 40320
+Hints:
+In case of input data being supplied to the question, it should be assumed to be a console input
+---------------------------------------------------------------------------------------------------------------------------------------
+
+ANSWER:
+=======
+
+******* simple method  ***************
+=============================================
+
+n=int(input("enter a no"))
+fact=1
+if n<0:
+    print("enter positive no")
+elif n==0:
+    print("factorial of 0 is 1")
+else:
+    for i in range(1,n+1):
+        fact=fact*i
+    print("Factorial of ",n," is :",fact)
+
+----------------------------------------------------------------------        
+
+********FACTORIAL USING RECURSION**************
+===============================================
+
+def fact(n):
+        if n==1 or n==0:
+            return 1
+        else:
+            return n*fact(n-1)  # call again to "fact" function
+    
+n=int(input("enter the number "))
+fact(n)
+print("Factorial of ",n," is : ",fact(n))
+
+
+=======================================================================================================================================
+
+Question 10.
+With a given integral number n, write a program to generate a dictionary that contains (i, i*i) 
+such that is an integral number between 1 and n (both included). and then the program should print the dictionary.
+Suppose the following input is supplied to the program: 8 Then,
+the output should be: {1: 1, 2: 4, 3: 9, 4: 16, 5: 25, 6: 36, 7: 49, 8: 64}
+Hints:
+In case of input data being supplied to the question, it should be assumed to be a console input. Consider use dict()
+---------------------------------------------------------------------------------------------------------------------------------------
+
+ANSWER:
+=======
+
+n=int(input("enter a no"))
+m=dict()
+for i in range(1,n+1):
+    m[i]=i*i
+print(m)
+
+=======================================================================================================================================
+
+Question 11.
+Write a program which accepts a sequence of comma-separated numbers from console and generate a list and a tuple which contains 
+every number. Suppose the following input is supplied to the program: 34,67,55,33,12,98 
+Then, the output should be: ['34', '67', '55', '33', '12', '98'] ('34', '67', '55', '33', '12', '98')
+Hints:
+In case of input data being supplied to the question, 
+it should be assumed to be a console input. tuple() method can convert list to tuple
+---------------------------------------------------------------------------------------------------------------------------------------
+ANSWER:
+=======
+
+n=input("enter the nos").split(",")
+p=list(n)
+q=tuple(n)
+print(p,q)
+
+=======================================================================================================================================
+
+Question 12.
+Define a class which has at least two methods: getString: to get a string from console input printString: to print the string 
+in upper case. Also please include simple test function to test the class methods.
+Hints:
+Use init method to construct some parameters
+---------------------------------------------------------------------------------------------------------------------------------------
+ANSWER:
+=======
+
+
+
+
+
+
+=======================================================================================================================================
+
+Question 12.
+Write a program that calculates and prints the value according to the given formula: Q = Square root of [(2 C D)/H] 
+Following are the fixed values of C and H: C is 50. H is 30. D is the variable whose values should be input to your 
+program in a comma-separated sequence. Example Let us assume the following comma separated 
+input sequence is given to the program: 100,150,180
+The output of the program should be: 18,22,24
+Hints:
+If the output received is in decimal form, it should be rounded off to its nearest value 
+(for example, if the output received is 26.0, it should be printed as 26) In case of input data being supplied to the question,
+it should be assumed to be a console input
+---------------------------------------------------------------------------------------------------------------------------------------
+ANSWER:
+=======
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
